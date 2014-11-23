8931c4024c11e02edbf642c68dbe9bce79161a24
kevsgrove <kevsgrove@gmail.com>
2014-11-22 19:11:18 -0800

    initial commit

---
+++ src/vwservlet/Bite.java
@@ -0,0 +1,44 @@
+package vwservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Bite
+ */
+@WebServlet("/Bite")
+public class Bite extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Bite() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String par1 =  request.getParameter("param1");
+		String par2 =  request.getParameter("param2");
+	    out.println(par1+"-"+par2);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/vwservlet/Player.java
@@ -0,0 +1,8 @@
+package vwservlet;
+
+public class Player {
+	static String id;
+	static boolean vamp;
+	static Integer vCount=0;
+	static Integer wCount=0;
+}
