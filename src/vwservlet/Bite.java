4d90c60f87d942e00770b6f212fb691aad0eed0a
kevsgrove <kevsgrove@gmail.com>
2014-11-23 05:52:42 -0800

    need database and yo things

--- src/vwservlet/Bite.java
+++ src/vwservlet/Bite.java
@@ -7,6 +7,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Servlet implementation class Bite
@@ -22,23 +27,45 @@ public class Bite extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    static final String dbURL = "jdbc:mysql://localhost:3306/yohackvw";
+    static final String username ="root";
+    static final String password = "yohack";
+    
+    protected void biteHandler(String p1, String p2){
+    	//sql query health of p2
+    	//if(health>10) health-=10;
+    	//else {
+    	//health=0;
+    	//sql query get team of p2
+    	//sql get users on p2's team
+    	//send yo to users
+    	//}
+    	//sql update health of p2
+    }
+    
+    protected void setPlayerId(String id, String youser){
+    	//sql query where yousername=youser
+    	//sql set that player's id
+    	//add youser to yo list
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		String par1 =  request.getParameter("param1");
-		String par2 =  request.getParameter("param2");
-	    if(par2.equals("-")){
+		String par1 =  request.getParameter("p1");
+		String par2 =  request.getParameter("p2");
+	    if(Character.isLowerCase(par2.charAt(0))){
 	    	out.println(par1);
-	    	//setPlayerId(par1, mobileNumber);
+	    	setPlayerId(par1, par2);
 	    }
 	    else if (par1.equals(par2))
 	    	out.println(par1+"-self bite");
 	    else {
 	    	out.println(par1+"-"+par2);
-	    	//biteHandler(par1, par2);
+	    	biteHandler(par1, par2);
 	    }
 		
 	}
